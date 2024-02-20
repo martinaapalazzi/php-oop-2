7d9755ada122f67ac9759d92bfeacb3cbf7e26c4
martinaapalazzi <martinaapalazzi@gmail.com>
2024-02-20 16:24:46 +0100

    v-for on cards

--- index.php
+++ index.php
@@ -68,6 +68,7 @@ echo '<h2> Card 3 </h2>'.
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <link rel="stylesheet" href="style.css">
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>PHP OOP Shop</title>
@@ -93,60 +94,66 @@ echo '<h2> Card 3 </h2>'.
                         Prodotti
                     </h2>
                 </div>
-                <div class="card me-3" style="width: 18rem;">
-                    <img src="<?php echo $productChickeNuggets->photo; ?>" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">
-                            <?php
-                                echo $productChickeNuggets->title;
-                            ?>
-                        </h5>
-                        <p class="card-text">
-                            <?php
-                                echo '<div>Price: '.$productChickeNuggets->price.'</div>';
-                            ?>
-                        </p>
-                        <a href="#" class="btn btn-primary">
-                            Go somewhere
-                        </a>
+                <?php
+                    foreach ($allProductsArray as $singleProduct) {
+                ?>
+                    <div class="card me-3" style="width: 18rem;">
+                        <div class="photo-product-container pt-4">
+                            <img src="<?php echo $singleProduct->photo; ?>" class="card-img-top" alt="...">
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                <?php
+                                    echo $singleProduct->title;
+                                ?>
+                            </h5>
+                            <p class="card-text">
+                                <div>
+                                    <?php
+                                        echo '<div>Price: '.$singleProduct->price.'</div>';
+                                    ?>
+                                </div>
+                                <div>
+                                    <?php
+                                        echo '<div> Category: '.$singleProduct->getCategory()->nameCategory.'</div>';
+                                    ?>
+                                </div>
+                                <?php
+                                    if (get_class($singleProduct) == 'Food') {
+                                ?>
+                                <div>
+                                    <?php
+                                        echo '<div> Diet: '.$singleProduct->diets.'</div>';
+                                    ?>
+                                </div>
+                                <?php 
+                                    }
+                                    else if (get_class($singleProduct) == 'Toys') {
+                                ?>
+                                <div>
+                                    <?php
+                                        echo '<div> Material: '.$singleProduct->material.'</div>';
+                                    ?>
+                                </div>
+                                <?php
+                                    }
+                                    else if (get_class($singleProduct) == 'Sleepingbags') {
+                                ?>
+                                <div>
+                                    <?php
+                                        echo '<div> Type: '.$singleProduct->type.'</div>';
+                                        echo '<div> Size: '.$singleProduct->size.'</div>';
+                                    ?>
+                                </div>
+                                <?php
+                                    }
+                                ?>
+                            </p>
+                        </div>
                     </div>
-                </div>
-                <div class="card me-3" style="width: 18rem;">
-                    <img src="<?php echo $productBall->photo; ?>" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">
-                            <?php
-                                echo $productBall->title;
-                            ?>
-                        </h5>
-                        <p class="card-text">
-                            <?php
-                                echo '<div>Price: '.$productBall->price.'</div>';
-                            ?>
-                        </p>
-                        <a href="#" class="btn btn-primary">
-                            Go somewhere
-                        </a>
-                    </div>
-                </div>
-                <div class="card" style="width: 18rem;">
-                    <img src="<?php echo $productDogBed->photo; ?>" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">
-                            <?php
-                                echo $productDogBed->title;
-                            ?>
-                        </h5>
-                        <p class="card-text">
-                            <?php
-                                echo '<div>Price: '.$productDogBed->price.'</div>';
-                            ?>
-                        </p>
-                        <a href="#" class="btn btn-primary">
-                            Go somewhere
-                        </a>
-                    </div>
-                </div>
+                <?php
+                    }
+                ?>
             </div>
         </div>
 
